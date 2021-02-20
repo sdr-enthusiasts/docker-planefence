7750f7c6418a7662a1b7d8f3fdc894ef774737fa
kx1t <ramonkolb@gmail.com>
2021-02-20 13:12:57 -0500

    add pf-fix

---
+++ planefence/scripts/pf-fix.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# fix for malformed csv file
+# note, this is a bandaid to correct the output of a buggy routine
+# and not a fix for the bug itself.
+# The issue it attempts to correct is that sometimes there is no URL
+# field written to the csv record causing it to collapse a field
+# This script corrects that.
+
+# get inputfile
+[[ ! -f "$1" ]] && CSV=/usr/share/planefence/html/planefence-$(date +%y%m%d).csv || CSV="$1"
+
+source /usr/share/planefence/planefence.conf
+[[ -f "$CSV.tmp" ]] && rm -f "$CSV.tmp"
+
+# find array length
+numrec=0
+while read -r l
+do
+	IFS=, read -ra r <<< "$l"
+	[[ ${#r[@]} -gt $numrec ]] && numrec=${#r[@]}
+done < "$CSV"
+
+while read -r l
+do
+	IFS=, read -ra r <<< "$l"
+	if [[ "${r[6]::4}" != "http" ]]
+	then
+		#we got a problem
+		IFS=, echo was: $l
+		for (( i=$numrec; i>=6; i-- ))
+		do
+			r[i]=${r[i-1]}
+		done
+
+		d=$(date -d "${l[2]}" +%s)
+                utcdate=$(date -u -d @"$d" +%Y-%m-%d)
+
+                printf -v url "http://globe.adsbexchange.com/?icao=%s&lat=%s&lon=%s&zoom=13&showTrace=%s" "${r[0]}" "$LAT" "$LON" "$utcdate"
+
+		r[6]=$url
+		l=""
+		for (( i=0; i<=$numrec; i++ ))
+		do
+			printf -v l "%s,%s" "$l" "${r[i]}"
+		done
+		l="${l:1}"
+		l="${l%,}"
+		echo now: $l
+	fi
+	echo $l >> "$CSV.tmp"
+
+done < "$CSV"
+
