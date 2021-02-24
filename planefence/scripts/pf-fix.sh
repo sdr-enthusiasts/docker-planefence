393982437ae68898f45d730022dbd6baee57d2bc
kx1t <ramonkolb@gmail.com>
2021-02-24 09:48:58 -0500

    Additional pf-fix rules added

--- planefence/scripts/pf-fix.sh
+++ planefence/scripts/pf-fix.sh
@@ -5,6 +5,11 @@
 # The issue it attempts to correct is that sometimes there is no URL
 # field written to the csv record causing it to collapse a field
 # This script corrects that.
+#
+# The desired CSV file layout is:
+# 0-ICAO,1-[@][Flight],2-date/time_first_heard,3-date/time_last_heard,
+# 4-min_alt,5-min_dist,6-webservicelink,7-[loudness],8-[peak_dB],9-[1min_dB],
+# 10-[5min_dB],11-[10min_dB],11-[1hr_dB],12-[tweetlink]
 
 # get inputfile
 [[ ! -f "$1" ]] && CSV=/usr/share/planefence/html/planefence-$(date +%y%m%d).csv || CSV="$1"
@@ -17,7 +22,7 @@ source /usr/share/planefence/planefence.conf
 
 # find array length
 numrec=0
-while read -r l
+while IFS= read -r l
 do
 	IFS=, read -ra r <<< "$l"
 	[[ ${#r[@]} -gt $numrec ]] && numrec=${#r[@]}
@@ -26,6 +31,8 @@ done < "$CSV"
 while read -r l
 do
 	IFS=, read -ra r <<< "$l"
+
+	# check for http link in field 6. If it doesn't exist, insert it
 	if [[ "${r[6]::4}" != "http" ]]
 	then
 		#we got a problem
@@ -36,9 +43,8 @@ do
 		done
 
 		d=$(date -d "${l[2]}" +%s)
-                utcdate=$(date -u -d @"$d" +%Y-%m-%d)
-
-                printf -v url "http://globe.adsbexchange.com/?icao=%s&lat=%s&lon=%s&zoom=13&showTrace=%s" "${r[0]}" "$LAT" "$LON" "$utcdate"
+    utcdate=$(date -u -d @"$d" +%Y-%m-%d)
+    printf -v url "http://globe.adsbexchange.com/?icao=%s&lat=%s&lon=%s&zoom=13&showTrace=%s" "${r[0]}" "$LAT" "$LON" "$utcdate"
 
 		r[6]=$url
 		l=""
@@ -50,6 +56,23 @@ do
 		l="${l%,}"
 		echo now: $l
 	fi
+
+	# there are less than 13 fields, then it's possible we have to relocate the twitter ID
+	# which may have been written into field 7
+	if [[ "${r[7]::13}" == "https://t.co/" ]]
+	then
+		r[8]=""
+		r[9]=""
+		r[10]=""
+		r[11]=""
+		r[12]="${r[7]}"
+		r[7]=""
+
+		printf -v l '%s,' "${r[@]}"
+		l="${l%,}"
+		echo now: $l
+	fi
+
 	echo $l >> "$CSV.tmp"
 
 done < "$CSV"
