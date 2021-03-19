a2a9b57b863ba06b35c20ee65822e1097b5bf34c
kx1t <15090643+kx1t@users.noreply.github.com>
2021-03-19 16:58:01 -0400

    Rel 5.3.1 (#45)
    * Rel5.0-official (#30)
    
    * added location fudging for the map
    
    * build command update
    
    * location fudging for maps
    
    * location fudging for maps
    
    * fixes for non-mile units
    
    * backout planefence.py changes
    
    * make ~/.planefence rwx upon creation
    
    * improvements for obtaining a tail or flight number from socket30003
    
    * minor change to rebase prep-planefence
    
    * minor typo fix
    
    * reduce logging
    
    * airlinename remote lookup as a last resort
    
    * latest
    
    * foutje bedankt
    
    * reducing remote lookup hits
    
    * remote cache lookup hits reduction
    
    * improvement to IGNORE DUPES to handle spaces in flight number field
    
    * another INGOREDUPES fix
    
    * cache bug fixed
    
    * cache bug fixed
    
    * minor issue fixed
    
    * remote cache optimization
    
    * added remote db check
    
    * caching fix
    
    * caching fix
    
    * exception for name lookup for HMED flight numbers (tnx @sportsbadger)
    
    * addtl exception work
    
    * airline names parsing cleanup
    
    * typo fix
    
    * bug fix
    
    * added built to hello string
    
    * fixed HMED cache issue
    
    * pre-release cleanup
    
    * pre-release cleanup
    
    * release 5.1
    
    * bug fix to prevent local cache dupes
    
    * bug fix to prevent local cache dupes
    
    * bug fix to prevent local cache dupes
    
    * retrieve airlinescodes from repo during cleanup runs
    
    * retrieve airlinescodes from repo during cleanup runs - update
    
    * Add icao 2 tail from cached mictronics database
    
    * Update planefence.conf
    
    * Add icao 2 tail from cached mictronics database
    
    * Add zip package for icao 2 tail from cached mictronics database
    
    * fixing stupid bug
    
    * bug file in mictronic db lookup
    
    * pf.sh lines 604+608: potential fix for dupes issue
    pf.sh line 598: "read -ra" -> "read -r"
    pt.sh line 136 added extra check for previous tweet
    
    * improved logging
    
    * major rewrite of record merging code, logging improvements
    
    * README updates
    
    * build script updates
    
    * don't print more than 5 decimals in LAT/LON on Planefence website
    
    * plane-alert custom fields-initial implementation
    
    * added custom field tweeting to plane-alert
    
    * fixed bug
    
    * major plane-alert bug fix
    
    * sorting fix
    
    * initial implementation of plane-alert range
    https://github.com/kx1t/docker-planefence/issues/39
    
    * add PF_PARANGE to default config file
    
    https://github.com/kx1t/docker-planefence/issues/39
    
    * restrict planetweet tweets to 280 chars (257+url)
    
    * implement link to ignore-list in planefence
    https://github.com/kx1t/docker-planefence/issues/36
    
    * changed planefence logging
    
    * logging change
    
    * added background image for PF and PA
    
    * fixed background stretching
    
    * added default background to build
    
    * dockerfile bug fix
    
    * typos
    
    * Update version number
    
    * Bug fix
    
    * Update plane-alert.sh
    
    * Update plane-alert.sh
    
    * changed directory structure of internal and persistent files
    
    * changed directory structure of internal and persistent files
    
    * cleanup and ensuring orderly transition to new directory structure
    
    * added explicit info on PF_CHECKREMOTEDB
    
    * typo
    
    * add customtags to plane-alert and planefence tweets
    
    * removed some warning messages when file already have been moved
    
    * add tags only if available
    
    * add tags only if available, config fixes
    
    * bug fix
    
    * added stripping of spaces
    
    * bug fix
    
    * bug fix
    
    * typo
    
    * typo

--- plane-alert/plane-alert.sh
+++ plane-alert/plane-alert.sh
@@ -180,10 +180,11 @@ then
 
 		# add a hashtag to the item if needed:
 		[[ "${header[0]:0:1}" == "$" ]] && pa_record[0]="#${pa_record[0]}" 	# ICAO field
-		[[ "${header[1]:0:1}" == "$" ]] && [[ "${pa_record[1]}" != "" ]]&& pa_record[1]="#${pa_record[1]}//[[:space:]]/" 	# tail field
+		[[ "${header[1]:0:1}" == "$" ]] && [[ "${pa_record[1]}" != "" ]]&& pa_record[1]="#${pa_record[1]//[[:space:]]/}" 	# tail field
 		[[ "${header[2]:0:1}" == "$" ]] && [[ "${pa_record[2]}" != "" ]]&& pa_record[2]="#${pa_record[2]//[[:space:]]/}" 	# owner field, stripped off spaces
 		[[ "${header[3]:0:1}" == "$" ]] && [[ "${pa_record[2]}" != "" ]] && pa_record[3]="#${pa_record[3]}" # equipment field
-		[[ "${header[1]:0:1}" == "$" ]] && [[ "${pa_record[8]}" != "" ]]&& pa_record[8]="#${pa_record[8]}//[[:space:]]/" # flight nr field (connected to tail header)
+		[[ "${header[1]:0:1}" == "$" ]] && [[ "${pa_record[8]}" != "" ]]&& pa_record[8]="#${pa_record[8]//[[:space:]]/}" # flight nr field (connected to tail header)
+
 		# First build the text of the tweet: reminder:
 		# 0-ICAO,1-TailNr,2-Owner,3-PlaneDescription,4-date,5-time,6-lat,7-lon
 		# 8-callsign,9-adsbx_url
