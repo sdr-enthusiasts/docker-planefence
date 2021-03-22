29dab9b2aa4406618abb20c04de8c4a2c4af0563
kx1t <ramonkolb@gmail.com>
2021-03-22 15:27:13 -0400

    new feature: dynamic plane-alert-db.txt loading

---
+++ planefence/services.d/get-pa-alertlist.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/with-contenv bash
+#shellcheck shell=bash
+
+APPNAME="$(hostname)/get-pa-alertlist"
+# -----------------------------------------------------------------------------------
+# Copyright 2020, 2021 Ramon F. Kolb - licensed under the terms and conditions
+# of GPLv3. The terms and conditions of this license are included with the Github
+# distribution of this package, and are also available here:
+# https://github.com/kx1t/planefence4docker/
+#
+# This package may incorporate other software and license terms.
+# -----------------------------------------------------------------------------------
+#
+# Make sure the /run directory exists
+echo "[$APPNAME][$(date)] get-pa-alertlist.sh started"
+#Get the list of alert files into ALERTLIST, or put the original file in it
+ALERTLIST="$(sed -n 's|^\s*ALERTLIST=\(.*\)|\1|p' /usr/share/plane-alert/plane-alert.conf)"
+[[ "$ALERTLIST" != "" ]] && IFS="," read -ra ALERTFILES <<< "$ALERTLIST" || ALERTFILES=("plane-alert-db.txt")
+
+# now iterate though them an put them in sequential files:
+rm -f /tmp/alertlist*.txt
+for ((i=0; i<"${#ALERTFILES[@]}"; i++))
+do
+	if [[ "${ALERTFILES[i]:0:5}" == "http:" ]] || [[ "${ALERTFILES[i]:0:6}" == "https:" ]]
+	then
+		# it's a URL and we need to CURL it
+		[[ "$(curl -L -s -fail -o /tmp/alertlist-$i.txt "${ALERTFILES[i]}" ; echo $?)" == "0" ]] && echo "[$APPNAME][$(date)] ALERTLIST ${ALERTFILES[i]} retrieval succeeded" || echo "[$APPNAME][$(date)] ALERTLIST ${ALERTFILES[i]} retrieval failed"
+	else
+		# it's a file and we need to concatenate it
+		if [[ -f "/usr/share/planefence/${ALERTFILES[i]}" ]]
+		then
+			cp -f "/usr/share/planefence/${ALERTFILES[i]}" /tmp/alertlist-$i.txt
+			echo "[$APPNAME][$(date)] ALERTLIST ${ALERTFILES[i]} retrieval succeeded"
+		else
+			echo "[$APPNAME][$(date)] ALERTLIST ${ALERTFILES[i]} retrieval failed"
+		fi
+	fi
+done
+
+awk -F',' '!seen[$1]++' /tmp/alertlist*.txt >/usr/share/planefence/persist/.internal/plane-alert-db.txt
+#rm -f /tmp/alertlist*.txt
