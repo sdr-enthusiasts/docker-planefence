202f7a52ed684afa506d1bac82c62fc19542ea48
kx1t <ramonkolb@gmail.com>
2021-03-22 12:08:08 -0400

    typo

--- plane-alert/plane-alert.sh
+++ plane-alert/plane-alert.sh
@@ -134,7 +134,7 @@ then
 
 		# clean up /tmp/patmp
 echo xx2 ; cat $TMPDIR/patmp
-		tac $TMP/patmp | sort -t',' -k1,1 -k9,9 -u  >> $TMPDIR/plalert.out.tmp # sort this from the reverse of the file
+		tac $TMPDIR/patmp | sort -t',' -k1,1 -k9,9 -u  >> $TMPDIR/plalert.out.tmp # sort this from the reverse of the file
 echo xx3 ; cat $TMPDIR/plalert.out.tmp
 		sort -t',' -k5,5 -k6,6 $TMPDIR/plalert.out.tmp > $TMPDIR/patmp
 echo xx4 ; cat $TMPDIR/patmp
