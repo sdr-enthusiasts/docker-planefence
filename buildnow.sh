d402b12fe2e66d5aa5d486f687c648cd7caa0a11
kx1t <15090643+kx1t@users.noreply.github.com>
2021-04-30 22:41:15 -0400

    build numbering update

--- buildnow.sh
+++ buildnow.sh
@@ -10,12 +10,13 @@ set -x
 # rebuild the container
 pushd ~/git/docker-planefence
 git checkout $BRANCH || exit 2
-
 git pull
 mv planefence/scripts/airlinecodes.txt /tmp
 curl -s -L -o planefence/scripts/airlinecodes.txt https://raw.githubusercontent.com/kx1t/planefence-airlinecodes/main/airlinecodes.txt
 
 export DOCKER_BUILDKIT=1
+
+echo "$(git branch --show-current)_($(git rev-parse --short HEAD))_$(date +%y-%m-%d-%T%Z)" > planefence/branch
 
 docker buildx build --compress --push $2 --platform linux/armhf,linux/arm64 --tag kx1t/planefence:$TAG .
 mv /tmp/airlinecodes.txt planefence/scripts/
