bbd5a3da625681f451bbfddb43bcba6d7702f845
kx1t <ramonkolb@gmail.com>
2021-07-12 11:19:16 -0400

    Minor bug fix for PF_ALERTHEADER feature

--- rootfs/usr/share/plane-alert/plane-alert.sh
+++ rootfs/usr/share/plane-alert/plane-alert.sh
@@ -261,8 +261,8 @@ comm -23 <(sort < "$OUTFILE") <(sort < /tmp/pa-old.csv ) >/tmp/pa-diff.csv
 
 # Read the header - we will need it a few times later:
 
-[[ "$PF_ALERTHEADER" != "" ]] && IFS="," read -ra header <<< "$(sed 's/\#\$/$#/g' <<< "$PF_ALERTHEADER")" || IFS="," read -ra header <<< "$(head -n1 "$PLANEFILE" | sed 's/\#\$/$#/g')"
-# if PF_ALERTHEADER is set, then use that one instead of
+[[ "$ALERTHEADER" != "" ]] && IFS="," read -ra header <<< "$(sed 's/\#\$/$#/g' <<< "$ALERTHEADER")" || IFS="," read -ra header <<< "$(head -n1 "$PLANEFILE" | sed 's/\#\$/$#/g')"
+# if ALERTHEADER is set, then use that one instead of
 
 [[ "$BASETIME" != "" ]] && echo "10d. $(bc -l <<< "$(date +%s.%2N) - $BASETIME")s -- plane-alert.sh: start Tweet run" || true
 
