e731d06667c38b2cb15361e9a1626aee63a8e6f4
Matthias Wirth <matthias.wirth@gmail.com>
2022-04-24 19:51:32 +0200

    fix logic around squawk / silhouette display
    the previous logic would not display the squawk banner when no
    silhouette was available, this didn't make sense

--- rootfs/usr/share/plane-alert/plane-alert.sh
+++ rootfs/usr/share/plane-alert/plane-alert.sh
@@ -635,36 +635,43 @@ do
 			fi
 		fi
 
-		if [[ -f /usr/share/planefence/html/plane-alert/$IMGURL ]]
+		if [[ "${pa_record[10]}" != "" ]]
 		then
-			# print aircraft silhouette or Squawk
-			if [[ "${pa_record[10]}" != "" ]]
-			then
-				# determine text color for squawk
-				case "${pa_record[10]}" in
-					"7700")
-						SQCOLOR="#7F0000"
+			# print Squawk
+
+			# determine text color for squawk
+			case "${pa_record[10]}" in
+				"7700")
+					SQCOLOR="#7F0000"
 					;;
-					"7600")
-						SQCOLOR="#FF6A00"
+				"7600")
+					SQCOLOR="#FF6A00"
 					;;
-					"7500")
-						SQCOLOR="#00194C"
+				"7500")
+					SQCOLOR="#00194C"
 					;;
-					"7400")
-						SQCOLOR="#2D3F00"
+				"7400")
+					SQCOLOR="#2D3F00"
 					;;
-					*)
-						SQCOLOR="#000000"
+				*)
+					SQCOLOR="#000000"
 					;;
-				esac
-				printf "    %s%s%s%s\n" "<td style=\"padding:0;\"><div style=\"vertical-align: middle; font-weight:bold; color:#D9EBF9; height:20px; text-align:center; line-height:20px; background:$SQCOLOR;\">" "<!-- img src=\"$IMGURL\" -->" "SQUAWK ${pa_record[10]}" "</div></td>" >&3
-			else
-				printf "    %s%s%s\n" "<td style=\"padding: 0;\"><div style=\"vertical-align: middle; font-weight:bold; color:#D9EBF9; height:20px; text-align:center; line-height:20px; background:none;\">" "<img src=\"$IMGURL\">" "</div></td>" >&3
-			fi
+			esac
+
+			printf "    %s%s%s\n" "<td style=\"padding:0;\"><div style=\"vertical-align: middle; font-weight:bold; color:#D9EBF9; height:20px; text-align:center; line-height:20px; background:$SQCOLOR;\">" "SQUAWK ${pa_record[10]}" "</div></td>" >&3
+
 		else
-			printf "    %s%s%s\n" "<td>" "" "</td>" >&3
+			# print aircraft silhouette if it exists
+			if [[ -f /usr/share/planefence/html/plane-alert/$IMGURL ]]
+			then
+				IMG="<img src=\"$IMGURL\">"
+			else
+				IMG=""
+			fi
+
+			printf "    %s%s%s\n" "<td style=\"padding: 0;\"><div style=\"vertical-align: middle; font-weight:bold; color:#D9EBF9; height:20px; text-align:center; line-height:20px; background:none;\">" "$IMG" "</div></td>" >&3
 		fi
+
 		printf "    <td><a href=\"%s\" target=\"_blank\">%s</a></td>\n" "${pa_record[9]}" "${pa_record[0]}" >>"$TMPDIR"/plalert-index.tmp # column: ICAO
 		printf "    <td><a href=\"%s\" target=\"_blank\">%s</a></td>\n" "https://flightaware.com/live/modes/${pa_record[0]}/ident/${pa_record[1]}/redirect" "${pa_record[1]}" >>"$TMPDIR"/plalert-index.tmp # column: Tail
 		#		printf "    %s%s%s\n" "<td>" "${pa_record[0]}" "</td>" >&3 # column: ICAO
