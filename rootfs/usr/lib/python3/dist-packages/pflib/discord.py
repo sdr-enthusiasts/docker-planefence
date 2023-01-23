73942197fca5ae0a18712a908ba5bff17f4edf73
kx1t <kx1t@amsat.org>
2023-01-23 16:24:27 -0500

    test build to see if Discord notifs will work again

--- rootfs/usr/lib/python3/dist-packages/pflib/discord.py
+++ rootfs/usr/lib/python3/dist-packages/pflib/discord.py
@@ -1,24 +1,46 @@
+# import discord_webhook as dw
+#
+# def build(username, urls, title, description, color=None):
+#     # If the Feeder Name is a link pull out the text:
+#     if '[' in username and ']' in username:
+#         username = username.split('[')[1].split(']')[0]
+#     webhooks = ()
+#     embeds = ()
+#     for url in urls:
+#         webhook = dw.DiscordWebhook(url=url, username=username)
+#
+#         if color is None:
+#             color = 0x007bff  # Blue
+#         embed = dw.DiscordEmbed(title=title, color=color, description=description)
+#         embed.set_footer(text="Planefence by kx1t - docker:kx1t/planefence")
+#
+#         webhook.add_embed(embed)
+#         webhooks = webhooks + (webhook,)
+#         embeds = embeds + (embed,)
+#
+#     return webhooks, embeds
+#
+# def field(embed, name, value, inline=None):
+#     if inline is None:
+#         inline = True
+#     embed.add_embed_field(name=name, value=value, inline=inline)
 import discord_webhook as dw
 
 def build(username, urls, title, description, color=None):
     # If the Feeder Name is a link pull out the text:
     if '[' in username and ']' in username:
         username = username.split('[')[1].split(']')[0]
-    webhooks = ()
-    embeds = ()
-    for url in urls:
-        webhook = dw.DiscordWebhook(url=url, username=username)
 
-        if color is None:
-            color = 0x007bff  # Blue
-        embed = dw.DiscordEmbed(title=title, color=color, description=description)
-        embed.set_footer(text="Planefence by kx1t - docker:kx1t/planefence")
+    webhook = dw.DiscordWebhook(url=urls, username=username)
 
-        webhook.add_embed(embed)
-        webhooks = webhooks + (webhook,)
-        embeds = embeds + (embed,)
+    if color is None:
+        color = 0x007bff  # Blue
+    embed = dw.DiscordEmbed(title=title, color=color, description=description)
+    embed.set_footer(text="Planefence by kx1t - docker:kx1t/planefence")
 
-    return webhooks, embeds
+    webhook.add_embed(embed)
+
+    return webhook, embed
 
 def field(embed, name, value, inline=None):
     if inline is None:
