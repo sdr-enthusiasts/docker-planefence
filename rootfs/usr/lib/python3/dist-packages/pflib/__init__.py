b17c2d9d907a85605fbae1bc9c58bf6395f8fd9a
Justin <dipierroj@gmail.com>
2023-01-24 19:19:52 -0500

    Fix config loading

--- rootfs/usr/lib/python3/dist-packages/pflib/__init__.py
+++ rootfs/usr/lib/python3/dist-packages/pflib/__init__.py
@@ -69,7 +69,7 @@ def load_config():
     }
 
     # Load config
-    pfdir = os.getenv("PLANEFENCEDIR", "/usr/share/planefence")
+    pfdir = os.getenv("PLANEFENCEDIR", "/usr/share/planefence/persist")
     config_path = f"{pfdir}/planefence.config"
     if exists(config_path):
         with open(config_path) as cfgfile:
