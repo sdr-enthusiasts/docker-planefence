bced23b1ac74065509644a6e87a4c6d3d21029c4
kx1t <15090643+kx1t@users.noreply.github.com>
2023-01-26 08:55:48 -0500

    Remove quotes from discord name

--- rootfs/usr/lib/python3/dist-packages/pflib/discord.py
+++ rootfs/usr/lib/python3/dist-packages/pflib/discord.py
@@ -4,11 +4,13 @@ def build(username, urls, title, description, color=None):
     # If the Feeder Name is a link pull out the text:
     if '[' in username and ']' in username:
         username = username.split('[')[1].split(']')[0]
+    if '\"' in username:
+        username = username.strip('\"')
 
     if color is None:
         color = 0x007bff  # Blue
     embed = DiscordEmbed(title=title, color=color, description=description)
-    embed.set_footer(text="Planefence by kx1t - docker:kx1t/planefence")
+    embed.set_footer(text="Planefence by kx1t - https://planefence.com")
 
     webhooks = dw.create_batch(urls=urls, username=username)
 
