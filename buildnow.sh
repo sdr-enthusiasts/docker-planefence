67ebfa847016ea82c833ba82372521fd5f628ba6
kx1t <15090643+kx1t@users.noreply.github.com>
2023-09-05 18:04:42 -0400

    Update buildnow.sh to point at sdr-enthusiasts

--- buildnow.sh
+++ buildnow.sh
@@ -5,14 +5,14 @@
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
 [[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
 
-BASETARGET1=ghcr.io/kx1t
-BASETARGET2=kx1t
+BASETARGET1=ghcr.io/sdr-enthusiasts
+#BASETARGET2=kx1t
 
 IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
-IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+#IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
 
 
-echo "press enter to start building $IMAGE1 and $IMAGE2 from $BRANCH"
+echo "press enter to start building $IMAGE1 from $BRANCH"
 
 #shellcheck disable=SC2162
 read
@@ -24,6 +24,6 @@ set -x
 git pull -a
 
 docker buildx build -f Dockerfile --compress --push $2 --platform $ARCHS --tag "$IMAGE1" .
-[[ $? ]] && docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE2 .
+# [[ $? ]] && docker buildx build --compress --push $2 --platform $ARCHS --tag $IMAGE2 .
 
 echo "Total build time: $(( $(date +%s) - starttime )) seconds"
