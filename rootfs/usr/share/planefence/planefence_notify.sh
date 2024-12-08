3b383ab1f5bfb7aa1e70832f6f73abb014bad75b
kx1t <kx1t@amsat.org>
2024-12-08 16:30:17 +0100

    quoting experiments

--- rootfs/usr/share/planefence/planefence_notify.sh
+++ rootfs/usr/share/planefence/planefence_notify.sh
@@ -412,7 +412,7 @@ then
 			mqtt_string+=(--client_id \""${MQTT_CLIENT_ID:-$(hostname)}"\")
 			if [[ -n "$MQTT_USERNAME" ]]; then mqtt_string+=(--username "$MQTT_USERNAME"); fi
 			if [[ -n "$MQTT_PASSWORD" ]]; then mqtt_string+=(--password "$MQTT_PASSWORD"); fi
-			mqtt_string+=(--message \'"${MQTT_JSON//\"/\\\"}"\')
+			mqtt_string+=(--message \'"${MQTT_JSON}"\')
 
 			# shellcheck disable=SC2068
 			outputmsg="$(mqtt ${mqtt_string[@]})"
