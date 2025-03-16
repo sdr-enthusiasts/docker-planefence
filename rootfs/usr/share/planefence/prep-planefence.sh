be76556234edbe0ac6a654b5d65e21d27a2d006f
kx1t <kx1t@amsat.org>
2025-03-16 15:56:06 +0100

    minor fix

--- rootfs/usr/share/planefence/prep-planefence.sh
+++ rootfs/usr/share/planefence/prep-planefence.sh
@@ -60,7 +60,7 @@ chmod u=rwx,go=rx \
 	/usr/share/planefence/html/plane-alert/silhouettes \
 	/usr/share/planefence/html/scripts
 ln -sf /usr/share/planefence/html/scripts /usr/share/planefence/html/plane-alert/scripts
-ln -sf /usr/share/planefence/persist/planepix/cache /usr/share/planefence/html/imgcache
+if [[ ! -e /usr/share/planefence/html/imgcache ]]; then ln -sf /usr/share/planefence/persist/planepix/cache /usr/share/planefence/html/imgcache; fi
 chmod a+rx /usr/share/planefence/html/imgcache
 if [[ -f /usr/share/planefence/persist/planefence.config ]]; then
 	set -o allexport
