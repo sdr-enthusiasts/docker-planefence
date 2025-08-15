1233ee8da012cc00e7775e1f7bbe8b84c56f0657
kx1t <kx1t@amsat.org>
2025-08-15 18:10:32 -0400

    minor update

--- rootfs/usr/share/planefence/prep-planefence.sh
+++ rootfs/usr/share/planefence/prep-planefence.sh
@@ -451,6 +451,7 @@ configure_planealert "TABLESIZE" "${PA_TABLESIZE:-50}"
 configure_planefence "SHOWIGNORE" "${PF_SHOWIGNORE:-false}"
 configure_planealert "SHOWIGNORE" "${PA_SHOWIGNORE:-false}"
 configure_planealert "EXCLUSIONS" "${PA_EXCLUSIONS}"
+touch /tmp/.force_pa_webpage_update	# this is used to force a Plane-Alert webpage update upon change of parameters
 #
 # ---------------------------------------------------------------------
 # Last thing - save the date we processed the config to disk. That way, if ~/.planefence/planefence.conf is changed,
